b28c98911148f0b12f454f407a0aebb05a5f3d3f
ibarguipas <ibarguipas@educacion.navarra.es>
2024-01-10 00:06:24 +0100

    acabado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.primeras_vistas"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.primeras_vistas"
